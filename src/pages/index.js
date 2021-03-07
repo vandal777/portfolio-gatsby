55f7799761622ada97184545c1abb14caa3c4edd
Leonardo Badilla <leopardolalia@gmail.com>
2021-03-07 17:36:41 +0100

    Add background img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,12 @@
 import * as React from "react"
 import 'nes.css/css/nes.min.css';
 import "@fontsource/press-start-2p"
+import styled from 'styled-components';
+import img from '../images/header-background-2.jpg';
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+const Header = styled.header`
+  background-image: url(${img});
+`;
 
 const docLink = {
   text: "Documentation",
@@ -129,7 +80,7 @@ const links = [
 // markup
 const IndexPage = () => {
   return (
-    <header id="home">
+    <Header>
         <div className="row banner">
             <div className="banner-text">
                 <h1 className="responsive-headline" style={{ textTransform: 'uppercase' }}>
@@ -162,7 +113,7 @@ const IndexPage = () => {
                 </ul>
             </div>
         </div>
-    </header>
+    </Header>
   )
 }
 
